0f27709e35410600a230fd262d1c9e0fb31e1ba0
Johann2112 <lovelyduel21@gmail.com>
2024-08-11 04:07:10 -0500

    enemigos pequeños, plataformas (prefabs), minionsenemies(code/gizmos)

--- Assets/Code/minionsenemies.cs
+++ Assets/Code/minionsenemies.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class minionsenemies : MonoBehaviour
 {
-    public float speed = 5f; 
-    public Vector2 punto1; 
-    public Vector2 punto2; 
+    public float speed = 5f;
+    public Vector2 punto1;
+    public Vector2 punto2;
 
     private Vector2 direction;
     private Vector2 moveD;
@@ -23,8 +23,18 @@ public class minionsenemies : MonoBehaviour
 
         if (transform.position.x > punto2.x || transform.position.x < punto1.x)
         {
-            direction = -direction; 
-            moveD = direction * speed; 
+            direction = -direction;
+            moveD = direction * speed;
         }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(punto1, 0.1f);
+        Gizmos.DrawSphere(punto2, 0.1f);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(punto1, punto2);
     }
 }
